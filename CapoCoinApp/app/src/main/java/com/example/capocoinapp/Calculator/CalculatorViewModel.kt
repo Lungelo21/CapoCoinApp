60fe450ac1343974a678c07451bdc17c63759ae3
GregC1905 <ST10441749@vcconnect.edu.za>
2026-04-24 17:52:23 +0200

    Started the calculator functionality, created a seperate folder for the calculator related models

---
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/Calculator/CalculatorViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.capocoinapp.Calculator
+
+import android.view.View
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+
+    var state by mutableStateOf(CalculatorState())
+
+        private set
+}
