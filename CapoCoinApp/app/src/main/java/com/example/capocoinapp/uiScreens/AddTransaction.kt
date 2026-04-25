146e3171b94e7b8e153747007ad5083d4b02b23e
GregC1905 <ST10441749@vcconnect.edu.za>
2026-04-25 10:45:22 +0200

    Added all rows and buttons for the calculator input for transaction amount.

--- CapoCoinApp/app/src/main/java/com/example/capocoinapp/uiScreens/AddTransaction.kt
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/uiScreens/AddTransaction.kt
@@ -19,8 +19,11 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.capocoinapp.Calculator.CalculatorFunctions
+import com.example.capocoinapp.Calculator.CalculatorOperation
 import com.example.capocoinapp.Calculator.CalculatorState
 import com.example.capocoinapp.designUI.components.CalculatorButton
+import com.example.capocoinapp.ui.theme.CardBG
+import com.example.capocoinapp.ui.theme.NavBarBG
 import com.example.capocoinapp.ui.theme.Primary
 
 @Composable
@@ -49,14 +52,16 @@ fun Calculator(
                 maxLines = 2
             )
 
+            // Row 1 of Calculator(AC, Del, /)
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
             ){
+                // Calculator button for AC
                 CalculatorButton(
                     symbol = "AC",
                     modifier = Modifier
-                        .background(Primary)
+                        .background(NavBarBG)
                         .aspectRatio(2f)
                         .weight(2f),
 
@@ -64,8 +69,253 @@ fun Calculator(
                         onAction(CalculatorFunctions.ClearFunction)
                     }
                 )
+                // Calculator button for Del
+                CalculatorButton(
+                    symbol = "Del",
+                    modifier = Modifier
+                        .background(Primary)
+                        .aspectRatio(1f)
+                        .weight(2f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.DeleteFunction)
+                    }
+                )
+                // Calculator button for /
+                CalculatorButton(
+                    symbol = "/",
+                    modifier = Modifier
+                        .background(Primary)
+                        .aspectRatio(2f)
+                        .weight(2f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Operation(CalculatorOperation.Divide))
+                    }
+                )
             }
 
+            // Row 2 of Calculator(7, 8, 9, x)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ){
+                // Calculator button for 7
+                CalculatorButton(
+                    symbol = "7",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(7))
+                    }
+                )
+
+                // Calculator button for 8
+                CalculatorButton(
+                    symbol = "8",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(8))
+                    }
+                )
+
+                // Calculator button for 9
+                CalculatorButton(
+                    symbol = "9",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(9))
+                    }
+                )
+
+                // Calculator button for x
+                CalculatorButton(
+                    symbol = "x",
+                    modifier = Modifier
+                        .background(Primary)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Operation(CalculatorOperation.Multiply))
+                    }
+                )
+            }
+
+            // Row 3 of Calculator(4, 5, 6, -)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ){
+                // Calculator button for 4
+                CalculatorButton(
+                    symbol = "4",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(4))
+                    }
+                )
+
+                // Calculator button for 5
+                CalculatorButton(
+                    symbol = "5",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(5))
+                    }
+                )
+
+                // Calculator button for 6
+                CalculatorButton(
+                    symbol = "6",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(6))
+                    }
+                )
+
+                // Calculator button for -
+                CalculatorButton(
+                    symbol = "-",
+                    modifier = Modifier
+                        .background(Primary)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Operation(CalculatorOperation.Subtract))
+                    }
+                )
+            }
+
+            // Row 4 of Calculator(1, 2, 3, +)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ){
+                // Calculator button for 1
+                CalculatorButton(
+                    symbol = "1",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(1))
+                    }
+                )
+
+                // Calculator button for 2
+                CalculatorButton(
+                    symbol = "2",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(2))
+                    }
+                )
+
+                // Calculator button for 3
+                CalculatorButton(
+                    symbol = "3",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(3))
+                    }
+                )
+
+                // Calculator button for +
+                CalculatorButton(
+                    symbol = "+",
+                    modifier = Modifier
+                        .background(Primary)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Operation(CalculatorOperation.Add))
+                    }
+                )
+            }
+
+            // Row 5 of Calculator(0, ., =)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(buttonSpacing)
+            ){
+
+                // Calculator button for 0
+                CalculatorButton(
+                    symbol = "0",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(2f)
+                        .weight(2f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.Number(0))
+                    }
+                )
+
+                // Calculator button for .
+                CalculatorButton(
+                    symbol = ".",
+                    modifier = Modifier
+                        .background(CardBG)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.DecimalFunction)
+                    }
+                )
+
+                // Calculator button for =
+                CalculatorButton(
+                    symbol = "=",
+                    modifier = Modifier
+                        .background(Primary)
+                        .aspectRatio(1f)
+                        .weight(1f),
+
+                    onClick = {
+                        onAction(CalculatorFunctions.CalculateFunction)
+                    }
+                )
+
+
+            }
         }
     }
 }
