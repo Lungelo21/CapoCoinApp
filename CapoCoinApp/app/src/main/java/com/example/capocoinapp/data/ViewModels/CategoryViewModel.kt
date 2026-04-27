546c039bcbefdf96a090bb766ee43dd83a2435e6
ST10441754 <ST10441754@myemeris.edu.za>
2026-04-27 11:24:15 +0200

    Updated the CategoryViewModel to reference the Category Service

--- CapoCoinApp/app/src/main/java/com/example/capocoinapp/data/ViewModels/CategoryViewModel.kt
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/data/ViewModels/CategoryViewModel.kt
@@ -6,13 +6,14 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.capocoinapp.Services.CategoryService
 import com.example.capocoinapp.data.dao.CategoryDAO
 import com.example.capocoinapp.data.entities.Category
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 class CategoryViewModel(
-    private val dao: CategoryDAO
+    private val service: CategoryService
 ) : ViewModel() {
 
     // UI Feedback message
@@ -38,7 +39,7 @@ class CategoryViewModel(
                         categoryColour = categoryColour,
                         categoryIcon = categoryIcon
                     )
-                    dao.insertCategory(newCategory)
+                    service.createCategory(newCategory)
                     "Category: '$categoryTitle' was added!"
                 }
             }
@@ -47,16 +48,16 @@ class CategoryViewModel(
 
     // Get all categories to populate your dropdown/selection menus
     fun getAllCategories(): Flow<List<Category>> {
-        return dao.getAllCategories()
+        return service.getAllCategories()
     }
 }
 
 // Factory to inject the CategoryDAO
-class CategoryViewModelFactory(private val dao: CategoryDAO) : ViewModelProvider.Factory {
+class CategoryViewModelFactory(private val service: CategoryService) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CategoryViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return CategoryViewModel(dao) as T
+            return CategoryViewModel(service) as T
         }
         throw IllegalArgumentException("Error Occurred")
     }
