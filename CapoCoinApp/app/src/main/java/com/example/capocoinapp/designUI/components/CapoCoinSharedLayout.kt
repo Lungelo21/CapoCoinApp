79bd7dd6dd561098f20c55aacb3f9f36b289e113
Abdullah449 <ST10450795@vcconnect.edu.za>
2026-04-28 18:50:25 +0200

    Fixed AppScaffolding rendering the nav bars over system bars

--- CapoCoinApp/app/src/main/java/com/example/capocoinapp/designUI/components/CapoCoinSharedLayout.kt
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/designUI/components/CapoCoinSharedLayout.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -189,6 +192,7 @@ fun AppScaffold(
     content: @Composable (PaddingValues) -> Unit
 ) {
     Scaffold(
+        contentWindowInsets = WindowInsets.systemBars,
         topBar = topBar,
         bottomBar = bottomBar,
         containerColor = BackgroundColor
@@ -233,6 +237,7 @@ fun TopNavBar(navController: NavController) {
                     bottomStart = 20.dp
                 )
             )
+            .statusBarsPadding()
     ) {
         Row(
             modifier = Modifier
