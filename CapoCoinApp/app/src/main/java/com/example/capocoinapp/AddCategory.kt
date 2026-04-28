3dc1d8c3f26fc220db8d6e3f2dc17ea5569f5811
ST10441754 <ST10441754@myemeris.edu.za>
2026-04-28 18:39:15 +0200

    Adjusting the AddCategory to  make the button to save the category be in the middle by wrapping the entry fields within a box and made sure that the txt for Save Category will only appear if  all 4 entry  fields are completed and disappear if not filled in

--- CapoCoinApp/app/src/main/java/com/example/capocoinapp/AddCategory.kt
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/AddCategory.kt
@@ -80,6 +80,7 @@ import androidx.compose.material.icons.Icons
 //importing question mark icon for default fallback icon
 import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -325,29 +326,35 @@ fun AddCategory(viewModel: CategoryViewModel, service: CategoryService, navContr
         //Spacing out the entry fields from the Add Category Button
         Spacer(modifier = Modifier.height(32.dp))
 
-        //Save button calling the Primary button method from Buttons.kt
-        PrimaryButton(
-            buttonText = if(transactionType.isNotBlank() &&
-                categoryTitle.isNotBlank() &&
-                iconColour.isNotBlank() &&
-                selectedIcon.isNotBlank())
-            {"Save Category"}
-            else{""},
-            enabled = transactionType.isNotBlank() &&
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center)
+        {
+            //Save button calling the Primary button method from Buttons.kt
+            PrimaryButton(
+                buttonText = if (transactionType.isNotBlank() &&
                     categoryTitle.isNotBlank() &&
                     iconColour.isNotBlank() &&
-                    selectedIcon.isNotBlank(),
-            onClick = {
-                viewModel.addCategory(
-                    type = transactionType,
-                    categoryTitle = categoryTitle,
-                    categoryColour = iconColour,
-                    categoryIcon = selectedIcon,
-                    minBudget = minBudget,
-                    maxBudget = maxBudget
-                )
-            }
-        )
+                    selectedIcon.isNotBlank()
+                ) {
+                    "Save Category"
+                } else {
+                    ""
+                },
+                enabled = transactionType.isNotBlank() &&
+                        categoryTitle.isNotBlank() &&
+                        iconColour.isNotBlank() &&
+                        selectedIcon.isNotBlank(),
+                onClick = {
+                    viewModel.addCategory(
+                        type = transactionType,
+                        categoryTitle = categoryTitle,
+                        categoryColour = iconColour,
+                        categoryIcon = selectedIcon,
+                        minBudget = minBudget,
+                        maxBudget = maxBudget
+                    )
+                }
+            )
+        }
     }
     //}
     //}
