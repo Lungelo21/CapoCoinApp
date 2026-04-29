fc59ca9c622daa8d9e78c891f7bb8f19b3fa346a
ST10441754 <ST10441754@myemeris.edu.za>
2026-04-29 14:52:58 +0200

    Formatting the CategoryTotals page with better indentation within the Date filtering functionality

--- CapoCoinApp/app/src/main/java/com/example/capocoinapp/CategoryTotals.kt
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/CategoryTotals.kt
@@ -125,166 +125,166 @@ fun CategoryTotalsScreen(service: TransactionService, navController: NavHostCont
 
     Box(modifier = Modifier.fillMaxSize())
     {
-    //UI Formatting
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp).verticalScroll(scrollState))
-    {
-        //Row for Date Selection
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp))
+        //UI Formatting
+        Column(modifier = Modifier.fillMaxSize().padding(16.dp).verticalScroll(scrollState))
         {
-            // Start Date Button
-            OutlinedButton(onClick = { showDatePicker(true) }, modifier = Modifier.weight(1f))
+            //Row for Date Selection
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp))
             {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start
-                )
+                // Start Date Button
+                OutlinedButton(onClick = { showDatePicker(true) }, modifier = Modifier.weight(1f))
                 {
-                    //Added an Icon to the Filter button for easier readability and usability
-                    Icon(
-                        imageVector = Icons.Default.CalendarMonth,
-                        contentDescription = null,
-                        modifier = Modifier.padding(end = 8.dp),
-                        tint = TextWhite
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start
                     )
-
-                    //Setting the text for the End Date Filter button and accounts when an end date is selected
-                    Text(
-                        text = if (startDate.isEmpty()) "Start Date" else "From: $startDate",
-                        color = TextWhite,
-                    )
-                }
-            }
-            // End Date Button
-            OutlinedButton(onClick = { showDatePicker(false) }, modifier = Modifier.weight(1f))
-            {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start
-                )
-                {
-                    //Added an Icon to the Filter button for easier readability and usability
-                    Icon(
-                        imageVector = Icons.Default.CalendarMonth,
-                        contentDescription = null,
-                        modifier = Modifier.padding(end = 8.dp),
-                        tint = TextWhite
-                    )
-
-                    //Setting the text for the End Date Filter button and accounts when an end date is selected
-                    Text(
-                        text = if (endDate.isEmpty()) "End Date" else "To: $endDate",
-                        color = TextWhite
-                    )
-                }
-            }
-        }
-
-        //Check to ensure Clear Filters button wont appear if no filter has been made
-        if(startDate.isNotEmpty() || endDate.isNotEmpty())
-        {
-            //Instantiating the button for the Clear Filter with empty values (no filter - all days)
-            OutlinedButton(onClick = {
-                startDate = ""
-                endDate = ""
-            },
-                //Making the button take up the fill width of the screen
-                modifier = Modifier.fillMaxWidth()
-            )
-            //Setting button's text
-            {
-                Text("Clear All filters", color = TextWhite)
-            }
-        }
-
-        //Adding a Spacer between the Category Totals and Date Filtering functionality
-        Spacer(modifier = Modifier.height(24.dp))
-
-
-        //Updated data when dates are changed
-        val totals by service.getCategoryTotals(startDate, endDate)
-            .collectAsState(initial = emptyList())
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 4.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            //Setting text for Category Name/Title Header
-            Text(
-                text = "Category",
-                style = MaterialTheme.typography.labelMedium,
-                color = Color.Gray,
-                fontWeight = FontWeight.Bold
-            )
-            //Setting text for Category Total Amount Header
-            Text(
-                text = "Total Amount",
-                style = MaterialTheme.typography.labelMedium,
-                color = Color.Gray,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-        //Using a Horizontal divider to space out Category and Total Amount values
-        HorizontalDivider(
-            modifier = Modifier.padding(horizontal = 8.dp),
-            thickness = 1.dp,
-            color = Color.LightGray.copy(alpha = 0.3f)
-        )
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-        )
-        {
-            //Checking if the totals are not populated
-            if(totals.isEmpty())
-            {
-                //Setting text to inform user of no Transactions found within relevant categories
-                Text(
-                    text = "No transactions found for this period.",
-                    color = Color.Gray,
-                    modifier = Modifier.padding(vertical = 20.dp, horizontal = 8.dp)
-                )
-            }
-            else
-            {
-                //Going through the list of saved total amounts for categories
-                totals.forEach { item ->
-
-                    //Mapping all items with the unique ID to represent each individual category and its total
-                    key(item.categoryTotalID)
                     {
-                        CategoryTotalRow(item)
+                        //Added an Icon to the Filter button for easier readability and usability
+                        Icon(
+                            imageVector = Icons.Default.CalendarMonth,
+                            contentDescription = null,
+                            modifier = Modifier.padding(end = 8.dp),
+                            tint = TextWhite
+                        )
+
+                        //Setting the text for the End Date Filter button and accounts when an end date is selected
+                        Text(
+                            text = if (startDate.isEmpty()) "Start Date" else "From: $startDate",
+                            color = TextWhite,
+                        )
+                    }
+                }
+                // End Date Button
+                OutlinedButton(onClick = { showDatePicker(false) }, modifier = Modifier.weight(1f))
+                {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start
+                    )
+                    {
+                        //Added an Icon to the Filter button for easier readability and usability
+                        Icon(
+                            imageVector = Icons.Default.CalendarMonth,
+                            contentDescription = null,
+                            modifier = Modifier.padding(end = 8.dp),
+                            tint = TextWhite
+                        )
+
+                        //Setting the text for the End Date Filter button and accounts when an end date is selected
+                        Text(
+                            text = if (endDate.isEmpty()) "End Date" else "To: $endDate",
+                            color = TextWhite
+                        )
                     }
                 }
             }
+
+            //Check to ensure Clear Filters button wont appear if no filter has been made
+            if(startDate.isNotEmpty() || endDate.isNotEmpty())
+            {
+                //Instantiating the button for the Clear Filter with empty values (no filter - all days)
+                OutlinedButton(onClick = {
+                    startDate = ""
+                    endDate = ""
+                },
+                    //Making the button take up the fill width of the screen
+                    modifier = Modifier.fillMaxWidth()
+                )
+                //Setting button's text
+                {
+                    Text("Clear All filters", color = TextWhite)
+                }
+            }
+
+            //Adding a Spacer between the Category Totals and Date Filtering functionality
+            Spacer(modifier = Modifier.height(24.dp))
+
+
+            //Updated data when dates are changed
+            val totals by service.getCategoryTotals(startDate, endDate)
+                .collectAsState(initial = emptyList())
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                //Setting text for Category Name/Title Header
+                Text(
+                    text = "Category",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = Color.Gray,
+                    fontWeight = FontWeight.Bold
+                )
+                //Setting text for Category Total Amount Header
+                Text(
+                    text = "Total Amount",
+                    style = MaterialTheme.typography.labelMedium,
+                    color = Color.Gray,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            //Using a Horizontal divider to space out Category and Total Amount values
+            HorizontalDivider(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                thickness = 1.dp,
+                color = Color.LightGray.copy(alpha = 0.3f)
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+            {
+                //Checking if the totals are not populated
+                if(totals.isEmpty())
+                {
+                    //Setting text to inform user of no Transactions found within relevant categories
+                    Text(
+                        text = "No transactions found for this period.",
+                        color = Color.Gray,
+                        modifier = Modifier.padding(vertical = 20.dp, horizontal = 8.dp)
+                    )
+                }
+                else
+                {
+                    //Going through the list of saved total amounts for categories
+                    totals.forEach { item ->
+
+                        //Mapping all items with the unique ID to represent each individual category and its total
+                        key(item.categoryTotalID)
+                        {
+                            CategoryTotalRow(item)
+                        }
+                    }
+                }
+            }
+
+            //Adding Space to the page for a Back to User Budget  ensuring that the button
+            //will not be covered by the Category totals
+            Spacer(modifier = Modifier.height(100.dp))
         }
 
-        //Adding Space to the page for a Back to User Budget  ensuring that the button
-        //will not be covered by the Category totals
-        Spacer(modifier = Modifier.height(100.dp))
-    }
+        Box(modifier = Modifier.padding(16.dp).fillMaxSize(), contentAlignment = Alignment.BottomCenter)
+        {
+            //Primary button call for a redirect to budget functionality
+            WideButton(
+                buttonText = "Back to Budget",
+                onClick = {
+                    // Navigates back to the Budget screen
+                    navController.navigate("UserBudget")
+                }
+            )
+        }
 
-    Box(modifier = Modifier.padding(16.dp).fillMaxSize(), contentAlignment = Alignment.BottomCenter)
-    {
-        //Primary button call for a redirect to budget functionality
-        WideButton(
-            buttonText = "Back to Budget",
-            onClick = {
-                // Navigates back to the Budget screen
-                navController.navigate("UserBudget")
-            }
-        )
+        //Add Spacer to ensure the button is away from the bottom edge from the screen
+        Spacer(modifier = Modifier.height(16.dp))
     }
-
-    //Add Spacer to ensure the button is away from the bottom edge from the screen
-    Spacer(modifier = Modifier.height(16.dp))
-}
 }
 
 //Method which dictates the Format and what goes in each Row
