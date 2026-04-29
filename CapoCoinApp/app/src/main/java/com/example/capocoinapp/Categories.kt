94f096482224a670f3dbffbfb02297f183b75f76
Lungelo21 <166373795+Lungelo21@users.noreply.github.com>
2026-04-29 15:00:02 +0200

    CategoryTotals button

--- CapoCoinApp/app/src/main/java/com/example/capocoinapp/Categories.kt
+++ CapoCoinApp/app/src/main/java/com/example/capocoinapp/Categories.kt
@@ -1,6 +1,10 @@
 package com.example.capocoinapp
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,12 +19,15 @@ import com.example.capocoinapp.designUI.components.CategoryCard
 import com.example.capocoinapp.designUI.components.TopNavBar
 import com.example.capocoinapp.ui.theme.CapoCoinAppTheme
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ShortNavigationBarDefaults.arrangement
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.capocoinapp.ui.theme.*
+
 
 @Composable
 fun CategoriesScreen(navController: NavController, categoryService: CategoryService) {
@@ -39,16 +46,37 @@ fun CategoriesScreen(navController: NavController, categoryService: CategoryServ
             CardBox(
                 cards = listOf(
                     {
-                        OutlinedButton(
-                            onClick = { navController.navigate("AddCategories")},
-                            border = BorderStroke(3.dp, Accent),
+                        Row (
+                            modifier = Modifier,
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ){// Add category button
+                            OutlinedButton(
+                                onClick = { navController.navigate("AddCategories") },
+                                border = BorderStroke(3.dp, Accent),
 
-                            colors = ButtonDefaults.outlinedButtonColors(
-                                containerColor = Color.Transparent,
-                                contentColor = Accent
-                            )
-                        ){
-                            Text("Add Category")
+                                colors = ButtonDefaults.outlinedButtonColors(
+                                    containerColor = Color.Transparent,
+                                    contentColor = Accent
+                                )
+                            ) {
+                                Text("Add Category")
+                            }
+
+                            Spacer(modifier= Modifier.width(40.dp) )
+
+                            // Category total button
+                            OutlinedButton(
+                                onClick = { navController.navigate("CategoryTotals")},
+                                border = BorderStroke(3.dp, Accent),
+
+                                colors = ButtonDefaults.outlinedButtonColors(
+                                    containerColor = Color.Transparent,
+                                    contentColor = Accent
+                                )
+                            ){
+                                Text("Category total")
+                            }
+
                         }
 
                         categories.forEach { category ->
